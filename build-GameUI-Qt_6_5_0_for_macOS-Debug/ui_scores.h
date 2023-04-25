f9d53fff92adb2f205a887b0d093a0785b71ee50
junming212900 <111775409+junming212900@users.noreply.github.com>
2023-04-25 17:03:45 -0600

    update

--- build-GameUI-Qt_6_5_0_for_macOS-Debug/ui_scores.h
+++ build-GameUI-Qt_6_5_0_for_macOS-Debug/ui_scores.h
@@ -25,17 +25,21 @@ public:
     QPushButton *mainMenuScoreBtn;
     QFrame *line_4;
     QLabel *topScoresLabel;
+    QLabel *backgroundLabel;
 
     void setupUi(QWidget *Scores)
     {
         if (Scores->objectName().isEmpty())
             Scores->setObjectName("Scores");
-        Scores->resize(853, 909);
+        Scores->resize(853, 733);
         label = new QLabel(Scores);
         label->setObjectName("label");
         label->setGeometry(QRect(190, 60, 461, 91));
-        label->setStyleSheet(QString::fromUtf8("font: 35pt \"Academy Engraved LET\";\n"
-"background-color: rgb(182, 75, 28);"));
+        label->setStyleSheet(QString::fromUtf8("QLabel {\n"
+"    font-family: \"Academy Engraved LET\";\n"
+"    font-size: 35pt;\n"
+"    background-color: rgb(182, 75, 28);\n"
+"}"));
         label->setFrameShape(QFrame::StyledPanel);
         label->setLineWidth(10);
         label->setTextFormat(Qt::RichText);
@@ -53,8 +57,30 @@ public:
         line_4->setFrameShadow(QFrame::Sunken);
         topScoresLabel = new QLabel(Scores);
         topScoresLabel->setObjectName("topScoresLabel");
-        topScoresLabel->setGeometry(QRect(290, 220, 311, 361));
+        topScoresLabel->setGeometry(QRect(260, 190, 311, 361));
         topScoresLabel->setMaximumSize(QSize(16777215, 361));
+        topScoresLabel->setStyleSheet(QString::fromUtf8("QLabel {\n"
+"    font-family: \"Arial\";\n"
+"    font-size: 24px;\n"
+"    font-weight: bold;\n"
+"    color: #333;\n"
+"    text-align: center;\n"
+"    background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #ffcc00, stop: 1 #f6ad42);\n"
+"    border: 2px solid #333;\n"
+"    border-radius: 5px;\n"
+"    padding: 8px;\n"
+"    margin: 4px;\n"
+"    text-shadow: 1px 1px 2px #666;\n"
+"}"));
+        backgroundLabel = new QLabel(Scores);
+        backgroundLabel->setObjectName("backgroundLabel");
+        backgroundLabel->setGeometry(QRect(7, 5, 831, 831));
+        backgroundLabel->setStyleSheet(QString::fromUtf8("border-image: url(/Users/junmingjin/MSD/CS6015/QTG/QtGame/GameUI/gameBkg.jpg) 0 0 0 0 stretch stretch;"));
+        backgroundLabel->raise();
+        label->raise();
+        mainMenuScoreBtn->raise();
+        line_4->raise();
+        topScoresLabel->raise();
 
         retranslateUi(Scores);
 
@@ -67,6 +93,7 @@ public:
         label->setText(QCoreApplication::translate("Scores", "Leaderboard", nullptr));
         mainMenuScoreBtn->setText(QCoreApplication::translate("Scores", "Main Menu", nullptr));
         topScoresLabel->setText(QCoreApplication::translate("Scores", "<html><head/><body><p><span style=\" font-size:64pt; font-weight:700; font-style:italic;\">TextLabel</span></p></body></html>", nullptr));
+        backgroundLabel->setText(QCoreApplication::translate("Scores", "TextLabel", nullptr));
     } // retranslateUi
 
 };
